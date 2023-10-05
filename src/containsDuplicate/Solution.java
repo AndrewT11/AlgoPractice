a01a9c9f7b8c778cf51c088ba5d73f8ba68b8dee
Andrew Tran <tranandrew@gmail.com>
2023-10-04 21:44:38 -0500

    contains duplicate solution using HashSet

--- src/containsDuplicate/Solution.java
+++ src/containsDuplicate/Solution.java
@@ -1,14 +1,27 @@
 package containsDuplicate;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Solution {
-    public static boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1]) {
+//    public static boolean containsDuplicate(int[] nums) {
+//        Arrays.sort(nums);
+//        for(int i = 1; i < nums.length; i++) {
+//            if (nums[i] == nums[i-1]) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
+    public static boolean containsDuplicate2(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if (seen.contains(nums[i])) {
                 return true;
             }
+            seen.add(nums[i]);
         }
         return false;
     }
@@ -17,6 +30,6 @@ public class Solution {
         int[] array = new int[]{1,1,1,3,3,4,3,2,4,2};
         int[] array2 = new int[]{1,2,3,4};
 
-        System.out.println(containsDuplicate(array2));
+        System.out.println(containsDuplicate2(array));
     }
 }
